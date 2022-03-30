804900d349965e10d6b9e10c174e34e60042365d
elparthka <parthka.2005@gmail.com>
2022-03-30 23:02:05 +0530

    add service container

--- logic.js
+++ logic.js
@@ -3,12 +3,34 @@ const proLine = document.getElementsByClassName("pro-line")[0]
 const proLineAlt = document.getElementsByClassName("pro-line-alt")[0]
 const docEle = document.documentElement
 
-web.onscroll = (e) => {
+/* set theme */
+const goDark = () => {
+    web.classList.add("theme-alt")
+    $.cookie("USER_THEME", "dark")
+}
+
+const goLight = () => {
+    web.classList.remove("theme-alt")
+    $.cookie("USER_THEME", "light")
+}
+
+if ($.cookie("USER_THEME")) {
+    if ($.cookie("USER_THEME") === 'dark') {
+        goDark()
+    } else {
+        goLight()
+    }
+} else {
+    goLight()
+}
+
+/* Scroll event */
+const setScroll = (e) => {
 
     let tH = docEle.scrollTop;
     let cP = docEle.scrollHeight - window.innerHeight
-    let lW = tH/cP * 100
-    
+    let lW = tH / cP * 100
+
     proLine.style.width = lW + "%";
     proLineAlt.style.width = lW + "%";
 
@@ -17,9 +39,10 @@ web.onscroll = (e) => {
     } else {
         web.classList.remove("alt")
     }
-    
+
 }
 
-const dark = () => {
-    web.classList.add("theme-alt")
-}
+setScroll()
+
+web.onscroll = setScroll
+
