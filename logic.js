27930ae24cf8f67ea98b3da98d0c3f6e34333aaf
elparthka <parthka.2005@gmail.com>
2022-03-30 12:21:54 +0530

    nav bar comoplite

---
+++ logic.js
@@ -0,0 +1,21 @@
+const web = document.body
+const proLine = document.getElementsByClassName("pro-line")[0]
+const proLineAlt = document.getElementsByClassName("pro-line-alt")[0]
+const docEle = document.documentElement
+
+web.onscroll = (e) => {
+
+    let tH = docEle.scrollTop;
+    let cP = docEle.scrollHeight - window.innerHeight
+    let lW = tH/cP * 100
+    
+    proLine.style.width = lW + "%";
+    proLineAlt.style.width = lW + "%";
+
+    if (docEle.scrollTop != 0) {
+        web.classList.add("alt")
+    } else {
+        web.classList.remove("alt")
+    }
+    
+}
